a607182c385c21bc6906b8ef29acdaef3e59e691
mayra aracelly lucero flores <may.lucero@alumnos.duoc.cl>
2019-11-16 22:40:24 -0300

    carrusel listo, footer listo

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Cliente
 
 class ClienteForm(forms.ModelForm):
+    contraseña = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model= Cliente
         fields= ['nombre', 'rut', 'email', 'celular', 'contraseña']
